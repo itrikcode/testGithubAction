f2d98fdb4adaddc7f43ebf6f97eed314d43bf07a
itrikcode <kumarviveklohaman@gmail.com>
2023-12-21 22:37:25 +0530

    new add sop

--- src/main/java/in/itsp/test/DemoApplication.java
+++ src/main/java/in/itsp/test/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Vivek ");
+		System.out.println("new test");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 

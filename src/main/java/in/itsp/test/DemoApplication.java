68ac702ea5d5bc802bb1aec79643366c82db76d0
itrikcode <kumarviveklohaman@gmail.com>
2023-12-21 18:01:43 +0530

    sop commit

--- src/main/java/in/itsp/test/DemoApplication.java
+++ src/main/java/in/itsp/test/DemoApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello Action Test");
-		SpringApplication.run(DemoApplication.class, args);
+ 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }

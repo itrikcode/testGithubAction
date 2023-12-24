368d9c49204dc48e4b466645b9b546be944b7555
itrikcode <kumarviveklohaman@gmail.com>
2023-12-24 23:04:23 +0530

    update some file

--- src/main/java/in/itsp/test/DemoApplication.java
+++ src/main/java/in/itsp/test/DemoApplication.java
@@ -1,20 +1,22 @@
 package in.itsp.test;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Vivek ");
-		System.out.println("new test");
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 	public RestTemplate restTemplate(){
 		return restTemplate();
 	}
+
 
 }
---
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE users(
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL
+);

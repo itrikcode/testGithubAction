1b8ee5a205e659e5f9a512723808723d9419b8e6
itrikcode <kumarviveklohaman@gmail.com>
2024-01-18 20:51:54 +0530

    add sop

--- src/main/java/in/itsp/test/DemoApplication.java
+++ src/main/java/in/itsp/test/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("in the main Class");
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
